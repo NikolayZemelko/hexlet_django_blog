d933abafde0ffa10aa03de79adc4aeddf737568c
NikolayZemelko <zemelko1989@gmail.com>
2023-06-04 13:21:23 +0300

    fix: Delete comments path

--- article/urls.py
+++ article/urls.py
@@ -4,5 +4,4 @@ from article import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='articles-index'),
     path('<int:id>/', views.ArticleView.as_view(), name='article'),
-    path('<int:article_id>/comments/', views.ArticleCommentsView.as_view(), name='comments'),
 ]
