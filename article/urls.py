34ef2d53cbc0f28f84489429a8af4069d21ca3fd
NikolayZemelko <zemelko1989@gmail.com>
2023-06-08 21:27:44 +0300

    feat: Add Edit path

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,7 @@ from article import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='articles-index'),
+    path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>/', views.ArticleView.as_view(), name='article'),
     path('create/', views.ArticleFormCreateView.as_view(), name='articles_create'),
 ]
